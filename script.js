59c87519ff732f27099369d4835afd1c7068aeb4
ClayData <claytonortiz96@gmail.com>
2020-01-05 20:24:59 -0600

    added vars for input prompts to use in function

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 var passLength = prompt("Choose a password length between 8 - 128 characters");
-var specialChars = prompt("Would you like special characters in your password?");
-var numericChars = prompt("Would you like numeric characters in your password?");
-var lowerChars = prompt("Would you like lowercase characters in you password?");
-var upperChars = prompt("Would you like uppercase characters in your password");
+    if (passLength >= 8 & passLength <= 128){
+    }
+    else{
+        alert("Must be between 8 - 128 characters, reload page")
+    }
 
+
+var specialChars = prompt("Would you like special characters in your password, Yes or No?");
+    if (specialChars.toLowerCase == yes){
+      var uniqueChars = "/!\'#\'$%&'()*+,-./:;<=>?@[\']\'^_`{|}~";
+    }
+
+var numericChars = prompt("Would you like numeric characters in your password, Yes or No?");
+    if (numericChars.toLowerCase == yes){
+        var numberChars = "0123456789"; 
+
+    }
+var lowerChars = prompt("Would you like lowercase characters in your password, Yes or No?");
+    if(lowerChars.toLowerCase == yes){
+        var lowCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    
+var upperChars = prompt("Would you like uppercase characters in your password, Yes or No?");
+    if(upperChars.toLowerCase == yes){
+        var upCase = "abcdefghijklmnopqrstuvwxyz";
+    }
+
+function passwordGenerator(passLength){
+
+}

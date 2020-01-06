d36be6dc591714dbf652c353771a7159d7644b13
ClayData <claytonortiz96@gmail.com>
2020-01-06 13:32:15 -0600

    got if statements to work by removing toLowerCase method, not sure why that worked

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var passLength = prompt("Choose a password length between 8 - 128 characters");
     if (passLength >= 8 & passLength <= 128){
     }
@@ -14,40 +15,37 @@ var numericChars = prompt("Would you like numeric characters in your password, Y
 var lowerChars = prompt("Would you like lowercase characters in your password, Yes or No?"); 
 
 var upperChars = prompt("Would you like uppercase characters in your password, Yes or No?");
+
+console.log(specialChars);
+console.log(numericChars);
+console.log(lowerChars);
+console.log(upperChars);
+
     
-    if(upperChars.toLowerCase == "yes"){
-        var upCase = "abcdefghijklmnopqrstuvwxyz";
-    }
-    else{
-        upCase = "";
-    }
+    function passwordGenerator(up, low, unique, numeral, pass){
+        var upCase = "";
+        var uniqueChars = "";
+        var numberChars = "";
+        var lowCase = "";
+        const yes = "yes";
 
-    if (specialChars.toLowerCase == "yes"){
-        var uniqueChars = "/!\'#\'$%&'()*+,-./:;<=>?@[\']\'^_`{|}~";
-      }
-    else{
-        uniqueChars = "";
-    }
-
-    if (numericChars.toLowerCase == "yes"){
-        var numberChars = "0123456789"; 
-    }
-    else{
-        numberChars = "";
-    }
-
-    if(lowerChars.toLowerCase == "yes"){
-        var lowCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    else{
-         lowCase = "";
-    }
-
-    function passwordGenerator(num){
-        var pasVal = "";
-        var charSet = (upCase + lowCase + numberChars + uniqueChars);
+        if(up == yes){
+            upCase += "abcdefghijklmnopqrstuvwxyz";
+        }
+        if(unique == yes){
+            uniqueChars += "/!\'#\'$%&'()*+,-./:;<=>?@[\']\'^_`{|}~";
+        }
+        if(numeral == yes){
+            numberChars += "0123456789"; 
+        }
+        if(low == yes){
+            lowCase += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
         
-        for(var i = 0, n = charSet.length; i < num; i++){
+        var pasVal = "";
+        charSet = (upCase + lowCase + numberChars + uniqueChars)
+        
+        for(var i = 0, n = charSet.length; i < pass; i++){
            pasVal += charSet.charAt(Math.floor(Math.random() * n));
            
         } 
@@ -55,6 +53,8 @@ var upperChars = prompt("Would you like uppercase characters in your password, Y
     }
 
 
+passwordGenerator(upperChars, lowerChars, specialChars, numericChars, numPass);
 
-passwordGenerator(numPass);
+
+
 

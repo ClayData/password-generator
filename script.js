8214ced5b2fbbb60b721c2c5a1fa2d394e119a35
ClayData <claytonortiz96@gmail.com>
2020-01-05 19:25:21 -0600

    created input prompts

---
+++ script.js
@@ -0,0 +1,6 @@
+var passLength = prompt("Choose a password length between 8 - 128 characters");
+var specialChars = prompt("Would you like special characters in your password?");
+var numericChars = prompt("Would you like numeric characters in your password?");
+var lowerChars = prompt("Would you like lowercase characters in you password?");
+var upperChars = prompt("Would you like uppercase characters in your password");
+
